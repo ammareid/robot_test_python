2b1bab3d1408dd97f04ed2934433e643ff42a850
ammareid <ammar.eid@gmail.com>
2023-12-09 22:34:49 +0300

    Update magicbot.py

--- magicbot.py
+++ magicbot.py
@@ -71,52 +71,57 @@ OBSTACLE_THRESHOLD = 500
 Right_angle_threshhold = 45
 Left_angle_threshhold = 315
 
-lidar = RPLidar('COM3',baudrate=115200)
+lidar = RPLidar('/dev/ttyUSB0')
 
 info = lidar.get_info()
 print(info)
 
 health = lidar.get_health()
 print(health)
-
-for scanNo, scan in enumerate(lidar.iter_scans()):
-    # right meaning 0 < angle < 45
-    # left meaning 315 < angle < 360
-    right_object_detected = False
-    left_object_detected = False
-
-
-    
-    for lineNo, line in enumerate(scan):
-        angle = line[1]
-        distance = line[2]
+while(True):
+    try:
+        for scanNo, scan in enumerate(lidar.iter_scans()):
+            # right meaning 0 < angle < 45
+            # left meaning 315 < angle < 360
+            right_object_detected = False
+            left_object_detected = False
         
-        if ( (0 < angle) and (angle < Right_angle_threshhold)) and (distance<OBSTACLE_THRESHOLD):
-            right_object_detected = True
-#             print("Right Detected. Angle:",angle, "Distance: ", distance)
-        elif ((Left_angle_threshhold < angle) and (angle < 360)) and (distance<OBSTACLE_THRESHOLD):
-            left_object_detected = True
+        
             
-    print("Right: ", right_object_detected, ", Left:", left_object_detected)
-    
-    
-    
-    if right_object_detected and left_object_detected:
-        #go_left(2)
-        print("Full left")
-        time.sleep(2)
-    elif right_object_detected :
-#         go_left(1.5)
-        print("Go left")
-        time.sleep(1.5)
-    elif left_object_detected:
-#         go_right(1.5) 
-        print("Go right")
-        time.sleep(1.5)
-    else :
-#         go_forward()
-        print("Forward")
-     
+            for lineNo, line in enumerate(scan):
+                angle = line[1]
+                distance = line[2]
+                
+                if ( (0 < angle) and (angle < Right_angle_threshhold)) and (distance<OBSTACLE_THRESHOLD):
+                    right_object_detected = True
+        #             print("Right Detected. Angle:",angle, "Distance: ", distance)
+                elif ((Left_angle_threshhold < angle) and (angle < 360)) and (distance<OBSTACLE_THRESHOLD):
+                    left_object_detected = True
+                    
+            print("Right: ", right_object_detected, ", Left:", left_object_detected)
+            
+            
+            
+            if right_object_detected and left_object_detected:
+                #go_left(2)
+                print("Full left")
+                time.sleep(2)
+            elif right_object_detected :
+        #         go_left(1.5)
+                print("Go left")
+                time.sleep(1.5)
+            elif left_object_detected:
+        #         go_right(1.5) 
+                print("Go right")
+                time.sleep(1.5)
+            else :
+        #         go_forward()
+                print("Forward")
+    except RPLidarException:
+        lidar.stop()
+        lidar.disconnect()
+        lidar = RPLidar('/dev/ttyUSB0')
+
         
     #if scanNo >= 100:
       #  break
